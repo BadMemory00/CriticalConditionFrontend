7a2dff7b8ea224c2d0b59851995d6545e1e362e6
lisho <medo.lashen00@gmail.com>
2022-03-11 17:16:06 +0200

    here we land

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <div id="app" class="container mt-4">
     <NavBar/>
+    <LandingContent/>
     <LandingFooter/>
+
     
   </div>
   
@@ -9,11 +11,14 @@
 
 <script>
 import NavBar from './components/navbar.vue'
+import LandingContent from './components/Content.vue'
 import LandingFooter from './components/Footer.vue';
 export default {
 components:{
   NavBar,
-  LandingFooter
+  LandingContent,
+  LandingFooter,
+  
 }
 
 }
