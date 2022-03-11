3147464fc1165f5e7a19c4ea7f44f34aa4511522
lisho <medo.lashen00@gmail.com>
2022-03-11 15:40:05 +0200

    Footer first version

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,17 @@
 <template>
   <div id="app" class="container mt-4">
     <NavBar/>
+    <LandingFooter/>
   </div>
 </template>
 
 <script>
 import NavBar from './components/navbar.vue'
+import LandingFooter from './components/Footer.vue';
 export default {
 components:{
-  NavBar
+  NavBar,
+  LandingFooter
 }
 
 }
