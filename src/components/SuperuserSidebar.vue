cd619eda177a953d8edefcd7cf4282bce7045810
alhassen agha <alhassenhamdy@yahoo.com>
2022-03-16 22:01:25 +0200

    added link to profile

--- src/components/SuperuserSidebar.vue
+++ src/components/SuperuserSidebar.vue
@@ -46,7 +46,9 @@
                             <li>
                                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
                                 <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"/><path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"/>
-                                </svg><a href="#">profile</a>
+                                </svg>                
+                                <router-link to="/superuser/profile">Profile</router-link>
+
                             </li>
                             <li>
                                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-box-arrow-right" 
