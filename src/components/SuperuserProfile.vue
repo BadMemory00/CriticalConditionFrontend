ac8313fd83fcf318b53347f81d46ef8820475864
Ibrahim2232000 <ibrahimsherif@std.mans.edu.eg>
2022-03-16 21:53:06 +0200

    profile

--- src/components/SuperuserProfile.vue
+++ src/components/SuperuserProfile.vue
@@ -1,21 +1,69 @@
 <template>
-     
-   <div style="margin-left:600px">
-       <div class="card" style="width: 18rem;">
-  <img class="card-img-top" src="..." alt="Card image cap">
-  <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-  </div>
-  <ul class="list-group list-group-flush">
-    <li class="list-group-item">Cras justo odio</li>
-    <li class="list-group-item">Dapibus ac facilisis in</li>
-    <li class="list-group-item">Vestibulum at eros</li>
-  </ul>
-  <div class="card-body">
-    <a href="#" class="card-link">Card link</a>
-    <a href="#" class="card-link">Another link</a>
-  </div>
-       </div>
-   </div>
+<div>
+
+        <div class="card" id="card1">
+            <div class="title">
+                <h4>Hospital information</h4>
+            </div>       
+             <div class="card" id="card2">
+                 <form>
+                     
+                     <label for="">Name</label><br>
+                     <input type="text" name="name"><br>
+                      <label for="username">Username</label><br>
+                     <input type="text" name="username"><br>
+                      <label for="password">Password</label><br>
+                     <input type="text" name="password"><br>
+                      <label for="specialty">Specialty</label><br>
+                     <input type="text" name="specialty"><br>
+                      <label for="address">Address</label><br>
+                     <input type="text" name="address"><br>
+                      <label for="phoneNumber">Phone number</label><br>
+                     <input type="text" name="phoneNumber">
+                 </form>
+             </div>
+        </div>
+</div>
 </template>
+<style >
+#card1{
+    margin-top: 90px;
+    margin-left:400px;
+    width: 450px;
+    height: 500px;
+    border-radius: 20px;
+    background-color: rgb(242, 242, 242);
+    border-style: none;
+}
+.title{
+    text-align: center;
+    margin-top: 20px;
+}
+#card2{
+    margin-top: 10px;
+    margin-left:80px;
+    width: 300px;
+    height: 400px;
+    border-radius: 20px;
+    border-style: none;
+    
+
+}
+label{
+    margin-top: 10px;
+    margin-left: 60px;
+    font-weight: bold;
+    
+    
+}
+input{
+    
+    margin-left: 60px;
+     background-color: rgb(242, 242, 242);
+    border-style: none;
+     
+
+}
+
+
+</style>
