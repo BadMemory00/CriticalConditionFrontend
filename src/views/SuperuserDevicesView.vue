bd29b32832e796783ef58c888d84afbefdfcc7bf
Ibrahim2232000 <ibrahimsherif@std.mans.edu.eg>
2022-03-17 14:51:01 +0200

    devices

---
+++ src/views/SuperuserDevicesView.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="">
+    <SuperuserNavbar />
+    <SuperuserSidebar />
+    <SuperuserDevices />
+     
+    
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import SuperuserSidebar from '@/components/SuperuserSidebar.vue' ;
+import SuperuserNavbar from '@/components/SuperuserNavbar.vue' ;
+import SuperuserDevices from  '@/components/SuperuserDevices.vue'
+
+export default {
+  name: 'SuperuserHomeView',
+  components: {
+    SuperuserSidebar,
+    SuperuserNavbar,
+    SuperuserDevices,
+     
+  }
+}
+</script>
